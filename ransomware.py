c13189df7fa7e2165ea427ac38ae25bb12dd57d2
colinseiler <colin_seiler@comcast.net>
2021-02-16 11:04:29 -0500

    Python Skeleton Code

---
+++ ransomware.py
@@ -0,0 +1,29 @@
+# Generates encryption keys
+def keys():
+    print("Key generated")
+    return
+
+# Loads files to be encrypted
+def targets():
+    print("Files loaded")
+    return
+
+# Encrypts loaded files
+def encrypt():
+    print("Files encrypted")
+    return
+
+# Deletes old files and clears encryption key from memory
+def nuke():
+    print("Old files deleted")
+    print("Key wiped")
+    return
+    
+
+# Main
+print("Program start")
+keys()
+targets()
+encrypt()
+nuke()
+print("Program done")
